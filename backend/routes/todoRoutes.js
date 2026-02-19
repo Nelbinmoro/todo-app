1aca8164e75c35a75cfee3808176e94cf8802860
Nelbin <nelbinnelson10@gmail.com>
2026-02-19 21:35:01 +0530

    Remove backend submodule and track backend as normal folder

---
+++ backend/routes/todoRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const Todo = require("../models/Todo");
+const router = express.Router();
+
+// Get all todos
+router.get("/", async (req, res) => {
+  const todos = await Todo.find().sort({ createdAt: -1 });
+  res.json(todos);
+});
+
+// Add todo
+router.post("/", async (req, res) => {
+  const todo = new Todo(req.body);
+  await todo.save();
+  res.status(201).json(todo);
+});
+
+// Update status
+router.put("/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(todo);
+});
+
+// Delete todo
+router.delete("/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Todo deleted" });
+});
+
+module.exports = router;
